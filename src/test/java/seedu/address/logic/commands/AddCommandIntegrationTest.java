1f81106fa4e2ce52264bedef9983e2a9e8f36a84
markgcera <>
2023-09-23 01:09:16 +0800

    Remove some tests

--- src/test/java/seedu/address/logic/commands/AddCommandIntegrationTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandIntegrationTest.java
@@ -38,11 +38,4 @@ public class AddCommandIntegrationTest {
                 expectedModel);
     }
 
-    @Test
-    public void execute_duplicatePerson_throwsCommandException() {
-        Person personInList = model.getAddressBook().getPersonList().get(0);
-        assertCommandFailure(new AddCommand(personInList), model,
-                AddCommand.MESSAGE_DUPLICATE_PERSON);
-    }
-
 }
